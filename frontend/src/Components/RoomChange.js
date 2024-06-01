665cb8e04d4508283ffe641c59933e3724b7ec37
Laxmikanth-060 <laxmikanthanubothula@gmail.com>
2024-06-01 20:30:49 +0530

    Tested changes update

--- frontend/src/Components/RoomChange.js
+++ frontend/src/Components/RoomChange.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function RoomChange() {
   return (
-    <div>
+    <div className='m-10'>
         <form>
-            <span>Current Room : </span><input/>
+              <span>
+              Current Room : 
+              </span>
+              <input/>
+
         </form>
     </div>
   )
